bc0fcf497b113fc2d68773e9f838d9b3d6dd4c1c
akarapon.ty <33413377+beraba@users.noreply.github.com>
2020-07-01 23:58:42 +0700

    'test2'

--- src/features/api/user/register.js
+++ src/features/api/user/register.js
@@ -4,6 +4,7 @@ const register = async (input) => {
     try {
         await model.register(input) 
         return true
+        console.log('test')
     } catch (error) {  return { error: error.details[0].message } }
 }
 
